28231a162802ca2e3bdf2d0e36f300ece7ab2cad
Maria Smirnova <94869912+SmirnovaMari@users.noreply.github.com>
2023-04-06 20:01:03 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 
 from scipy.stats import norm
+from scipy.stats import t
 
 
 chat_id = 1444511418 # Ваш chat ID, не меняйте название переменной
@@ -10,8 +11,9 @@ def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
+    n = len(x)
     alpha = 1 - p
-    loc = (2*x/(26**2)).mean()
-    scale = np.sqrt(2*np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    t_alpha_2 = t.ppf(1 - alpha / 2, n - 1)
+    lower = loc - t_alpha_2 * scale
+    upper = loc + t_alpha_2 * scale
+    return (lower, upper)
